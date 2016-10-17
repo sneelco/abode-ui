bbc38e0e291bbb38a6d8e90db0e85483bd085f6b
Scott Neel <scott@neel.net>
2016-10-16 17:08:30 -0700

    * add initial ui for notifications

--- src/scripts/notifications.js
+++ src/scripts/notifications.js
@@ -1,5 +1,38 @@
 var notifications = angular.module('abode.notifications', ['ngResource']);
 
+notifications.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.when('/notifications', '/notifications/list');
+
+  $stateProvider
+  .state('main.notifications', {
+    url: '/notifications',
+    templateUrl: '/views/notifications/notifications.html',
+  })
+  .state('main.notifications.list', {
+    url: '/list',
+    templateUrl: '/views/notifications/notifications.list.html',
+    controller: 'notificationsList'
+  })
+  .state('main.notifications.add', {
+    url: '/add',
+    templateUrl: '/views/notifications/notifications.add.html',
+    controller: 'notificationsAdd'
+  })
+  .state('main.notifications.edit', {
+    url: '/:id',
+    templateUrl: '/views/notifications/notifications.edit.html',
+    controller: 'notificationsEdit',
+    resolve: {
+      'notification': ['$stateParams', '$state', 'Notifications', function ($stateParams, $state, Notifications) {
+
+        return Notifications.get({'id': $stateParams.id}).$promise;
+
+      }]
+    }
+  });
+});
+
 notifications.factory('Notifications', ['$resource', '$http', '$q', '$uibModal', 'abode', function ($resource, $http, $q, $uibModal, abode) {
 
   var model = $resource(abode.url('/api/notifications/:id/:action'), {id: '@_id'}, {
@@ -84,3 +117,74 @@ notifications.directive('notifications', [function () {
   };
 }]);
 
+notifications.controller('notificationsList', ['$scope', 'Notifications', function ($scope, Notifications) {
+
+  $scope.notifications = Notifications.query();
+
+  $scope.add = function () {
+
+  };
+
+}]);
+
+notifications.controller('notificationsAdd', ['$scope', '$state', 'abode', 'Notifications', function ($scope, $state, abode, Notifications) {
+
+  $scope.saving = false;
+  $scope.notification = new Notifications({'actions': [], 'triggers': []});
+
+
+  $scope.add = function () {
+    $scope.saving = true;
+
+    $scope.notification.$save().then(function () {
+      $scope.saving = false;
+      abode.message({'type': 'success', 'message': 'Notification Added'});
+
+      $state.go('^.list');
+    }, function (err) {
+      $scope.saving = false;
+      abode.message({'type': 'failed', 'message': 'Failed to Add Notification', 'details': err});
+      $scope.errors = err;
+    });
+
+  };
+
+}]);
+
+notifications.controller('notificationsEdit', ['$scope', '$state', 'abode', 'notification', function ($scope, $state, abode, notification) {
+
+  $scope.notification = notification;
+  $scope.saving = false;
+  $scope.deleting = false;
+
+  $scope.save = function () {
+    $scope.saving = true;
+
+    $scope.notification.$update().then(function () {
+      $scope.saving = false;
+      abode.message({'type': 'success', 'message': 'Notification Updated'});
+    }, function (err) {
+      $scope.saving = false;
+      abode.message({'type': 'failed', 'message': 'Failed to Update Notification', 'details': err});
+      $scope.errors = err;
+    });
+
+  };
+
+  $scope.delete = function () {
+
+    $scope.deleting = true;
+
+    $scope.notification.$delete().then(function () {
+      $scope.deleting = false;
+      abode.message({'type': 'success', 'message': 'Notification Deleted'});
+
+      $state.go('^.list');
+    }, function (err) {
+      $scope.deleting = false;
+      abode.message({'type': 'failed', 'message': 'Failed to Delete Notification', 'details': err});
+      $scope.errors = err;
+    });
+  };
+
+}]);
