853b4b7cb740a2da62b147d03b4b49c04823f9f4
Scott Neel <scott@neel.net>
2016-10-26 20:12:05 -0700

    * add action builder

--- src/scripts/notifications.js
+++ src/scripts/notifications.js
@@ -55,9 +55,83 @@ notifications.factory('Notifications', ['$resource', '$http', '$q', '$uibModal',
     return defer.promise;
   };
 
+  model.prototype.$triggers = function () {
+    var self = this,
+      defer = $q.defer(),
+      url = abode.url('/api/notifications/' + this._id + '/triggers').value();
+
+    $http.get(url).then(function (results) {
+      defer.resolve(results.data);
+    }, function (err) {
+      defer.reject(err.data);
+    });
+
+    return defer.promise;
+  };
+
+  model.prototype.$add_trigger = function (trigger) {
+    var self = this,
+      defer = $q.defer(),
+      url = abode.url('/api/notifications/' + this._id + '/triggers').value();
+
+    $http.post(url, {'_id': trigger._id || trigger}).then(function (results) {
+      defer.resolve();
+    }, function (err) {
+      defer.reject(err.data);
+    });
+
+    return defer.promise;
+  };
+
+  model.prototype.$remove_trigger = function (trigger) {
+    var self = this,
+      defer = $q.defer(),
+      trigger_id = trigger._id || trigger;
+      url = abode.url('/api/notifications/' + this._id + '/triggers/' + trigger_id).value();
+
+    $http.delete(url).then(function (results) {
+      defer.resolve();
+    }, function (err) {
+      defer.reject(err.data);
+    });
+
+    return defer.promise;
+  };
+
+  model.prototype.$add_action = function (action) {
+    var self = this,
+      defer = $q.defer(),
+      url = abode.url('/api/notifications/' + this._id + '/actions').value();
+
+    $http.post(url, action).then(function (result) {
+      defer.resolve(result.data);
+    }, function (err) {
+      defer.reject(err.data);
+    });
+
+    return defer.promise;
+  };
+
+  model.prototype.$remove_action = function (action) {
+    var self = this,
+      defer = $q.defer(),
+      action_id = action._id || action;
+      url = abode.url('/api/notifications/' + this._id + '/actions/' + action_id).value();
+
+    $http.delete(url).then(function (results) {
+      defer.resolve();
+    }, function (err) {
+      defer.reject(err.data);
+    });
+
+    return defer.promise;
+  };
+
   return model;
 
 }]);
+
+
 
 notifications.directive('notifications', [function () {
   return {
@@ -151,11 +225,186 @@ notifications.controller('notificationsAdd', ['$scope', '$state', 'abode', 'Noti
 
 }]);
 
-notifications.controller('notificationsEdit', ['$scope', '$state', 'abode', 'notification', function ($scope, $state, abode, notification) {
+notifications.controller('notificationsEdit', ['$scope', '$state', '$uibModal', 'abode', 'notification', function ($scope, $state, $uibModal, abode, notification) {
 
   $scope.notification = notification;
   $scope.saving = false;
   $scope.deleting = false;
+  $scope.loading = false;
+
+  $scope.load_triggers = function () {
+    $scope.loading = true;
+    $scope.notification.$triggers().then(function (results) {
+      $scope.loading = false;
+      $scope.triggers = results;
+
+    }, function () {
+      $scope.loading = false;
+    });
+  };
+
+  $scope.load_triggers();
+
+  $scope.add_action = function () {
+    var picker = $uibModal.open({
+      animation: false,
+      templateUrl: 'views/notifications/action.builder.html',
+      size: 'lg',
+      controller: ['$scope', '$uibModalInstance', 'Devices', 'Scenes', 'Rooms', 'notification', function ($scope, $uibModalInstance, Devices, Scenes, Rooms, notification) {
+        $scope.loading = true;
+        $scope.action = {};
+        $scope.notification = notification;
+        $scope.devices = Devices.query();
+        $scope.scenes = Scenes.query();
+        $scope.rooms = Rooms.query();
+        $scope.item = {};
+        $scope.type_args = [];
+
+        $scope.action_types = [
+          {name: 'Device', value: 'devices', icon: 'glyphicon glyphicon-oil'},
+          {name: 'Room', value: 'rooms', icon: 'glyphicon glyphicon-modal-window', capabilities: ['light', 'dimmer', 'conditioner']},
+          {name: 'Scene', value: 'scenes', icon: 'icon-picture', capabilities: ['light']},
+        ];
+
+        $scope.type_actions = [
+          {name: 'On', value: 'on', arguments: [], capabilities: ['light', 'dimmer', 'display', 'fan', 'onoff']},
+          {name: 'Off', value: 'off', arguments: [], capabilities: ['light', 'dimmer', 'display', 'fan', 'onoff']},
+          {name: 'Level', value: 'set_level', arguments: ['level'], capabilities: ['dimmer']},
+          {name: 'Status', value: 'status', arguments: [], capabilities: ['light', 'dimmer', 'display', 'fan', 'onoff']},
+          {name: 'Mode', value: 'set_mode', arguments: ['mode'], capabilities: ['conditioner']},
+          {name: 'Temperature', value: 'set_point', arguments: ['temperature'], capabilities: ['conditioner']},
+        ];
+
+        $scope.changeType = function (t) {
+          $scope.action.type = t;
+        };
+
+        $scope.changeItem = function (i) {
+          $scope.item = i;
+          $scope.action.name = i.name;
+        };
+
+        $scope.change_action = function(a) {
+          $scope.action.action = a.value;
+          $scope.type_args = a.arguments;
+          $scope.action.args = [];
+        };
+
+        var get_type = function (t) {
+          var matches = $scope.action_types.filter(function (i) {
+            return (i.value === t);
+          });
+
+          if (matches.length === 1) {
+            return matches[0];
+          }
+        };
+
+        $scope.has_capability = function (c) {
+          var capabilities = [];
+          var type = get_type($scope.action.type);
+
+          if (type && type.capabilities) {
+            capabilities = type.capabilities;
+          } else if (type && type.value === 'devices' && $scope.action.name) {
+            capabilities = $scope.item.capabilities || [];
+          }
+
+          var has = false;
+
+          capabilities.forEach(function (capability) {
+            if (c.indexOf(capability) !== -1) {
+              has = true;
+            }
+          });
+
+          return has;
+        };
+
+        $scope.close = function () {
+          $uibModalInstance.dismiss();
+        };
+
+        $scope.save = function() {
+          $scope.notification.$add_action($scope.action).then(function (result) {
+            $uibModalInstance.close(result);
+            abode.message({'type': 'success', 'message': 'Action Added'});
+          }, function () {
+            abode.message({'type': 'failed', 'message': 'Failed to add trigger'});
+          });
+        };
+      }],
+      resolve: {
+        notification: function () {
+          return $scope.notification;
+        }
+      }
+    });
+
+    picker.result.then(function (action) {
+      console.dir(action);
+      $scope.notification.actions.push(action);
+    });
+  };
+
+  $scope.remove_action = function (action) {
+    $scope.notification.$remove_action(action).then(function () {
+      $scope.notification.actions.splice($scope.notification.actions.indexOf(action), 1);
+      abode.message({'type': 'success', 'message': 'Action Removed'});
+    }, function (err) {
+      abode.message({'type': 'failed', 'message': 'Failed to remove action'});
+    })
+  };
+
+  $scope.add_trigger = function () {
+    var picker = $uibModal.open({
+      animation: false,
+      templateUrl: 'views/notifications/triggers.picker.html',
+      size: 'sm',
+      controller: ['$scope', '$uibModalInstance', 'Triggers', 'notification', function ($scope, $uibModalInstance, Triggers, notification) {
+        $scope.loading = true;
+        $scope.triggers = [];
+        $scope.notification = notification;
+
+        $scope.close = function () {
+          $uibModalInstance.dismiss();
+        };
+
+        $scope.select = function(trigger) {
+          $scope.notification.$add_trigger(trigger).then(function () {
+            $uibModalInstance.close(trigger);
+          }, function () {
+            abode.message({'type': 'failed', 'message': 'Failed to add trigger'});
+          });
+        };
+
+        Triggers.query().$promise.then(function (results) {
+          $scope.triggers = results;
+          $scope.loading = false;
+        });
+      }],
+      resolve: {
+        notification: function () {
+          return $scope.notification;
+        }
+      }
+    });
+
+    picker.result.then(function (trigger) {
+      $scope.notification.triggers.push(trigger._id);
+      $scope.load_triggers();
+    });
+  };
+
+  $scope.remove_trigger = function (trigger) {
+    $scope.notification.$remove_trigger(trigger).then(function () {
+      $scope.load_triggers();
+      $scope.notification.triggers.splice($scope.notification.triggers.indexOf(trigger._id), 1);
+      abode.message({'type': 'success', 'message': 'Trigger Removed'});
+    }, function (err) {
+      abode.message({'type': 'failed', 'message': 'Failed to remove trigger'});
+    })
+  };
 
   $scope.save = function () {
     $scope.saving = true;
