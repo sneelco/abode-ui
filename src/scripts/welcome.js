82b9cd39bc73c2faece9f5e36d18c71e2f836531
Scott Neel <scott@neel.net>
2016-12-29 10:22:21 -0800

    * add power popover

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -44,6 +44,30 @@ welcome.config(['$stateProvider', '$urlRouterProvider', function($state, $urlRou
 
 }]);
 
+welcome.controller('powerController', ['$scope','$http', 'abode', function ($scope, $http, abode) {
+  $scope.working = false;
+
+  $scope.restart = function () {
+    $scope.working = true;
+    $http.post('/api/abode/restart').then(function () {
+
+    }, function () {
+      $scope.working = false;
+      abode.message({'type': 'failed', 'message': 'Error restarting'});
+    });
+  };
+
+  $scope.shutdown = function () {
+    $scope.working = true;
+    $http.post('/api/abode/shutdown').then(function () {
+
+    }, function () {
+      $scope.working = false;
+      abode.message({'type': 'failed', 'message': 'Error restarting'});
+    });
+  };
+}]);
+
 welcome.controller('welcomeController', ['$scope', '$timeout', '$interval', '$http', '$q', '$state', 'abode', 'Auth', 'Interfaces', function ($scope, $timeout, $interval, $http, $q, $state, abode, Auth, Interfaces) {
 
   var ssl_checker;
