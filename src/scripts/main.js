42b3b1cc589989b42162d3dd4f567180a857bee1
Scott Neel <scott@neel.net>
2016-12-30 10:01:04 -0800

    * add tag directive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -146,6 +146,62 @@ abode.directive('iconSelector', ['$compile', function () {
   };
 }]);
 
+abode.directive('tags', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      tagModel: '='
+    },
+    templateUrl: 'views/main/tags.html',
+    controller: ['$scope', '$uibModal', function ($scope, $uibModal) {
+
+      $scope.tagModel = $scope.tagModel || [];
+
+      $scope.removeTag = function (index) {
+        $scope.tagModel.splice(index, 1);
+      };
+
+      $scope.addTag = function () {
+        $uibModal.open({
+          animation: false,
+          templateUrl: 'views/main/tags.add.html',
+          size: 'sm',
+          controller: ['$scope', '$uibModalInstance', function ($uiScope, $uibModalInstance) {
+
+            $uiScope.error = '';
+            $uiScope.tag = {'name': undefined};
+
+            $uiScope.add = function () {
+              if ($uiScope.tag.name === '' || $uiScope.tag.name === undefined) {
+                $uiScope.error = 'Tag not specified';
+                return;
+              }
+              var matches = $scope.tagModel.filter(function (tag) {
+                return (tag.toLowerCase() === $uiScope.tag.name.toLowerCase())
+              });
+
+              if (matches.length > 0) {
+                $uiScope.error = 'Tag already exists';
+                return
+              }
+
+              $uiScope.error = '';
+              $scope.tagModel.push($uiScope.tag.name);
+
+              $uibModalInstance.close();
+            }
+
+            $uiScope.cancel = function () {
+              $uibModalInstance.dismiss();
+            }
+          }]
+        });
+      }
+    }]
+  }
+});
+
 abode.config(['$stateProvider', '$urlRouterProvider', 'abodeProvider', function($state, $urlRouter, abode) {
 
   abode.load();
