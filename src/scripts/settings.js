d4538ff8fc3b1e9214df4a0e03269c7becd292af
Scott Neel <scott@neel.net>
2017-01-02 10:26:44 -0800

    * cleanup pin mgmt

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -230,6 +230,7 @@ settings.controller('pinsEdit', ['$scope', '$state', 'abode', 'triggers', 'confi
 
   $scope.save = function () {
     $scope.pin.$update().then(function () {
+      $scope.pin.pin = undefined;
       abode.message({'type': 'success', 'message': 'Pin saved successfully'});
     }, function (err) {
       abode.message({'type': 'failed', 'message': err.data.message || err.data || 'Failed to add Pin'});
